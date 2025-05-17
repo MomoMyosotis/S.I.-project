b2ba97afd2e87f701b645fcc463eaeb804891322
Myosotis <molollo73@gmail.com>
2025-05-17 03:35:28 +0200

    Delete Code/UI directory

--- Code/UI/home_page.py
+++
@@ -1,7 +0,0 @@
-#include user_operation.py , ricerca.py
-#pulsante Modifica Profilo --> funct modifica \ user_operation.py
-#pulsante ricerca --> funct ricerca.py
-#pulsante upload --> funct upload \ songs_operation.py 
-# #pulsante log-out --> funct log-out \ user_operation.py
-#pulsante elimina utenti --> funct Gestione_utenti \ ricerca.py(ADMIN)
-#pulsante richieste accesso --> funct richieste \ user_operation.py (ADMIN)
