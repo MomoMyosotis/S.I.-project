6c8241d9640d7de9d994c51744f2f2dacaf184e5
Myosotis <molollo73@gmail.com>
2025-05-21 16:54:30 +0200

    Delete Code/functions directory

--- Code/functions/modifica.py
+++
@@ -1,3 +0,0 @@
-#mette a schermo le info del proprio account: nome, cognome, mail, username, passworld, data_nascita, BIO
-#pulsante modifica info --> funct mod_info
-#pulsante elimina profilo --> funct elimina_profilo
